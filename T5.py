1d4413daa124787d697285bb266db1fa55f7dda5
unknown <adham.ghazali@gmail.com>
2022-05-28 12:03:39 +0300

    add configs

--- T5.py
+++ T5.py
@@ -1,15 +1,11 @@
 import torch
 from transformers import T5Tokenizer, T5EncoderModel, AutoTokenizer, AutoModelForSeq2SeqLM
+from configs import T5_CONFIGS
 
-T5_CONFIGS = {'t5-small': ['t5',512] ,'t5-base':['t5',768],
-              't5-large':['t5',1024] , 'google/t5-v1_1-small': ['auto',512],
-              'google/t5-v1_1-base':['auto', 768],
-              'google/t5-v1_1-large':['auto',1025]}
 
 MAX_LENGTH=256 # tokens
 
 def get_encoded_text(texts, model_name='t5-small'):
-    global T5_CONFIGS
     if model_name not in T5_CONFIGS.keys():
         print('model name is not found in config')
 
@@ -60,8 +56,8 @@ def test():
     model_name='t5-small'
     encoded_text,attention_masks= get_encoded_text(Texts, model_name)
     print("multi sentence test")
-
     Texts=["I love rock and roll",'especially the kind of stuff that rocks']
     encoded_text, attention_masks = get_encoded_text(Texts, model_name)
 
 
+test()
---
+++ configs.py
@@ -0,0 +1,8 @@
+T5_CONFIGS = {'t5-small': ['t5', 512], 't5-base': ['t5', 768],
+              't5-large': ['t5', 1024], 'google/t5-v1_1-small': ['auto', 512],
+              'google/t5-v1_1-base': ['auto', 768],
+              'google/t5-v1_1-large': ['auto', 1025]}
+
+
+
+
