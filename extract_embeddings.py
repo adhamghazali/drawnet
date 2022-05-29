61c787b512e761cfcbaabfaa7b1e54e8b5dba8a6
unknown <adham.ghazali@gmail.com>
2022-05-29 20:05:03 +0300

    save_encoded_text_to_numpy_files

--- extract_embeddings.py
+++ extract_embeddings.py
@@ -4,26 +4,14 @@ import numpy as np
 from tqdm import tqdm
 import os
 
-csvfile = r'D:\laion400m\laion_data.csv'
-
-
-
-
 def load_model():
     model_name = 't5-small'
     encoder = T5.t5encoder(model_name)
     return encoder
 
-def extract_embeddings():
-    return -1#extracted_embeddings
-
-def save_to_file(embeddings,filename,embeddigns_folder):
-    #np.save()
-    return -1
-
 
 if __name__ == "__main__":
-    load_model()
+    encoder=load_model()
     csvfile = r'D:\laion400m\laion_data_0.csv'
     embeddings_save_filename = r'D:\laion400m\embeddings\\'
     if not os.path.exists(embeddings_save_filename):
@@ -33,9 +21,7 @@ if __name__ == "__main__":
 
     for i in tqdm(range(len(df))):
         sentence = df['text'][i]
-        print(sentence)
-
-
-
-
+        fnx=embeddings_save_filename+str(i)
+        enocoded_text,_=encoder.get_encoded_text(sentence,save_to_file=True)
+        np.save(fnx, enocoded_text.cpu())
 
